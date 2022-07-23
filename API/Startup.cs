17151802a4cafbc8d963c9e349645425b78d2be3
torrance ross <torranceross@ymail.com>
2022-07-23 02:09:39 -0400

    client side skeleton completed fetching users from the server

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,11 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(policy => 
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
