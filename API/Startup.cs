fbeff99dc735e11e961010cade0c7502da04e1ee
torrance ross <torranceross@ymail.com>
2022-07-24 09:50:23 -0400

    client side registration added

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,11 +1,4 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
