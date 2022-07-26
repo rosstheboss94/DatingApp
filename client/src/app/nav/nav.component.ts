ed686f837acf91342c4ef42dae3b7e2f84a83461
torrance ross <torranceross@ymail.com>
2022-07-26 08:50:09 -0400

    client side error handling

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../services/account.service';
@@ -10,24 +12,25 @@ import { AccountService } from '../services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  
 
-  constructor(public accountService: AccountService) {}
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
-        console.log(response);
+        this.router.navigateByUrl('/members');
       },
-      error: (error) => console.log(error),
     });
   }
 
-  logout(){
+  logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
